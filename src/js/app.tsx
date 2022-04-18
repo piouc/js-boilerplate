43f2269705d4319e784b23a3f4f48c95cc1caa3c
piou <piou@hotmail.co.jp>
2022-04-18 13:03:24 +0900

    Update dependencies

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { FunctionComponent } from 'react';
 import { ResetCss } from './components/reset-css';
 import { RootCss } from './components/root-css';
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { App } from './app'
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('root')
   if(rootElement){
-    render(<App />, rootElement)
+    const root = createRoot(rootElement)
+    root.render(<App />)
   }
 })
